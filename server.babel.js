cba59de17da317ef8e900b1024199c11c05c77fd
Emmanuel Mendoza <emmanuel.mendoza@4thsource.com>
2017-01-23 18:46:28 -0600

    Applying server hmr - incomplete

--- server.babel.js
+++ server.babel.js
@@ -1,9 +1,14 @@
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotServerMiddleware from 'webpack-hot-server-middleware';
+import config from './webpack.server.config';
 import routermgr from './src/infra/route-manager';
 
 const app = express();
+const compiler = webpack(config);
 
 // Setting Express to use EJS templating
 app.set('view engine', 'ejs');
@@ -13,6 +18,10 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 // Middleware
 app.use(bodyParser.json());                         // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//HMR
+app.use(webpackDevMiddleware(compiler));
+app.use(webpackHotServerMiddleware(compiler));
 
 routermgr.handle(app);
 
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const fs = require('fs');
 
-console.log(path.resolve('.'));
-
 const nodeModules = fs.readdirSync('node_modules')
   .filter(function(x) {
     return ['.bin'].indexOf(x) === -1;
