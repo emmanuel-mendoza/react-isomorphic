8505b4d5189784fe1413b31ed1b305daa2ed14ac
Emmanuel Mendoza <emmanuel.mendoza@4thsource.com>
2017-05-09 18:37:21 -0500

    Jest added for async code

---
+++ src/data/data-manager.test.js
@@ -0,0 +1,26 @@
+import * as datamgr from './data-manager';
+
+const todos = [{text: "my first todo", date:1490064923485}]; //should be the same content as todos.json
+let testTodo = { text: "new TODO", date: Date.now() };
+
+it('get all TODOS', () => {
+    expect.assertions(1);
+    // return datamgr.getAll(datamgr.filename, 1).then(data => expect(data).toEqual([{text: "my first todo", date:1490064923485}]));
+    return expect(datamgr.getAll(datamgr.filename, 1)).resolves.toEqual(todos);
+});
+
+it('post new TODO', () => {
+    expect.assertions(1);
+    return expect(datamgr.post(datamgr.filename, testTodo, 1)).resolves.toEqual(testTodo);
+});
+
+testTodo.text = "updated";
+it('update last TODO', () => {
+    expect.assertions(1);
+    return expect(datamgr.update(datamgr.filename, testTodo, todos.length, 1)).resolves.toEqual(testTodo);
+});
+
+it('delete last TODO', () => {
+    expect.assertions(1);
+    return expect(datamgr.remove(datamgr.filename, todos.length, 1)).resolves.toEqual({ id: todos.length });
+});
