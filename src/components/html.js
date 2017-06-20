512e4f307fba61633e44f1366946a502d1861c84
Emmanuel <emmanuel@MacBook-de-Jorge.local>
2017-06-19 22:05:15 -0500

    Using React to build the HTML served instead of JS Templating

---
+++ src/components/html.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Html = ({js, css, html, initialState}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <title>Universal React-Redux Simple TO-DO List</title>
+      <meta name="theme-color" content="#ffffff" />
+      {css.map(css => <link key={css} rel="stylesheet" href={`/static/${css}`} />)}
+    </head>
+    <body>
+      <div
+        id="react-view"
+        dangerouslySetInnerHTML={{
+          __html: html
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.__INITIAL_STATE__=${JSON.stringify(initialState)}`
+        }}
+      />
+      {js.map(js => <script key={js} src={`/static/${js}`} />)}
+    </body>
+  </html>
+);
+
+export default Html;
--- src/infra/route-manager.js
+++ src/infra/route-manager.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import { StaticRouter, matchPath } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import Routes, { routes } from '../components/routes';
 import configureStore from '../store';
 import history from '../history';
-import Html from '../views';
+import Html from '../components/html';
 
 // Promises to gather all the data and dispatch it.
 const fetchComponentData = (dispatch, components, params) => {
@@ -44,9 +44,35 @@ function getCss(assetsByChunkName) {
   return [getCssByChunkName('client', assetsByChunkName)].filter(isTruthy);
 };
 
+const render = (location, store, stats) => {
+  // Rendering the main markup (the server counter-part of /client/index.js)
+  let context={};
+  const markup = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={location} context={context}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
+
+  // Getting the assets
+  const css = getCss(stats.assetsByChunkName);
+  const js=["bundle.js"];
+  const initialState = store.getState();
+
+  return renderToStaticMarkup(
+    <Html
+      js={js}
+      css={css}
+      html={markup}
+      initialState={initialState}
+    />
+  );
+};
+
 const router = (stats) => {
   const assetsByChunkName = stats.clientStats.assetsByChunkName;
-  const css = getCss(assetsByChunkName);
+  //const css = getCss(assetsByChunkName);
 
   return (req, res, next) => {
     console.log('URL: ', req.url, ' Date: ', Date.now());
@@ -63,23 +89,12 @@ const router = (stats) => {
     const store = configureStore(undefined, history);  
     fetchComponentData(store.dispatch, [match.component], match.params)
       .then((val) => {
-        const context = {};
-        const markup = renderToString(
-          <Provider store={store}>
-            <StaticRouter location={req.url} context={context}>
-              <Routes />
-            </StaticRouter>
-          </Provider>
-        );
-
-        // Request needs to be redirected if context.url is defined
-        if (context.url) {
-          res.redirect(301, context.url);
-        // Send rendered matched route with data to client
-      } else {
-          const html = Html(store.getState(), css, markup);
-          res.status(200).send(html);
-        }
+        // TO-DO: Need to research how to handle re-direction
+        //if (context.url) res.redirect(301, context.url);
+        
+        //const html = Html(store.getState(), css, markup);
+        const html=render(req.url, store, {assetsByChunkName});
+        res.status(200).send(`<!doctype html>${html}`);
     })
     .catch((err) => res.end(err.message));
 
