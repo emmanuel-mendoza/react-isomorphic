984a970f54ff30f700ba2bc2c26d679f7ef192a7
Emmanuel <emmanuel@MacBookdeJorge.lan>
2017-07-04 19:43:39 -0500

    server and server.babel files switched for better filename meaning

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-require('babel-register');
+import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+// HMR - Starts
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackHotServerMiddleware from 'webpack-hot-server-middleware';
+import config from './webpack.config';
+// HMR - Ends
+import apimgr from './src/infra/api-manager';
+import routesmgr from './src/infra/route-manager';
 
-// Prevent babel-register from transpiling css files on server side.
-require.extensions['.css'] = () => {
-    return;
-};
+const app = express();
+// app.use(express.static('public'));
 
-require('./server.babel');
+// Middleware
+app.use(bodyParser.json());                         // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// We use the API middleware first as we are not hot reloading it.
+app.use(apimgr);
+
+// HMR - Starts
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  quiet: false,
+  publicPath: config[0].output.publicPath,
+  stats: { colors: true }
+}));
+app.use(webpackHotMiddleware(compiler.compilers.find((compiler) => compiler.name === 'client'), {
+  log: () => {}
+}));
+app.use(webpackHotServerMiddleware(compiler, {
+  chunkName: 'server'
+}));
+// HMR - Ends
+
+// app.use(routesmgr);
+
+app.listen(3000, () => {
+  console.log('Server listening at port 3000');
+});
